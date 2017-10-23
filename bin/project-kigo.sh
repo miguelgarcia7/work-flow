14381c66a409a138c7827c55c62e1723a4fb2b6e
Miguel Angel Garcia <miguel@miguelangelgarcia.com>
2017-10-23 09:54:39 -0500

    new projects

--- bin/project-kigo.sh
+++ bin/project-kigo.sh
@@ -4,12 +4,15 @@ osascript &>/dev/null <<EOF
     tell application "iTerm"
         activate
         tell current window to set tb to create tab with default profile
-        tell current session of current window to write text "cd ~/Sites/kigo.net"
+        tell current session of current window to write text "cd ~/code/kigo.net"
         tell current session of current window to write text "gs"
 
         tell current window to set tb to create tab with default profile
-        tell current session of current window to write text "cd ~/Sites/kigo.net/resources/themes/kigo"
+        tell current session of current window to write text "cd ~/code/kigo.net/resources/themes/kigo"
         tell current session of current window to write text "gulp watch"
 
     end tell
 EOF
+
+cd ~/code/kigo.net
+subl .
--- bin/project-propertyware.sh
+++ bin/project-propertyware.sh
@@ -4,15 +4,15 @@ osascript &>/dev/null <<EOF
     tell application "iTerm"
         activate
         tell current window to set tb to create tab with default profile
-        tell current session of current window to write text "cd ~/code/realpage.com"
+        tell current session of current window to write text "cd ~/code/propertyware.com"
         tell current session of current window to write text "gs"
 
         tell current window to set tb to create tab with default profile
-        tell current session of current window to write text "cd ~/code/realpage.com/resources/themes/realpage"
+        tell current session of current window to write text "cd ~/code/propertyware.com/resources/themes/kigo"
         tell current session of current window to write text "gulp watch"
 
     end tell
 EOF
 
-cd ~/code/realpage.com
+cd ~/code/propertyware.com
 subl .
